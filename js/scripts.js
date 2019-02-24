fd5598f97493da314aa300fc5026f4e8d5362315
prudence naliaka <prudencenaliaka@gmail.com>
2019-02-24 09:58:38 +0300

    made changes in my scripts.js file made changes in index.html file

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
-var answers=["a library of js","append","Document Object Model","front-end logic","slideUp,fadeIn,delay"]
-var markPerCorrect=5;
+var answers=["a library of js","append","Document Object Model","front-end logic","slideUp,fadeIn,delay"];
+var scorePerCorrect=5;
 function percentage(){
-return "yourscore is" + parseInt(score/25) * 100 + "%";
+return "yourscore is" + parseInt((score/25) * 100) + "%";
 }
 
 $("document").ready(function(){
@@ -17,17 +17,19 @@ $("document").ready(function(){
     if(answerOne===undefined || answerTwo===undefined || answerThree===undefined || answerFour=undefined || answerFive=undefined){
       $("#questionsIncomplete").text("work out all questions first");
       $("#questionsComplete").slideUp();
-    }else if(answerOne===answers[0]){
-      score += markPerCorrect;
-    }else if(answerTwo===answers[1]){
-      score += markPerCorrect;
-    }else if(answerThree===answers[2]){
-      score += markPerCorrect;
-    }else if(answerFour===answers[3]){
-      score += markPerCorrect;
-    }else(answerFive===answers[4]){
-      score += markPerCorrect;
-    }
+    }else
+      {  if(answerOne===answers[0]){
+      score += scorePerCorrect;
+        }if(answerTwo===answers[1]){
+      score += scorePerCorrect;
+        }if(answerThree===answers[2]){
+      score += scorePerCorrect;
+        }if(answerFour===answers[3]){
+      score += scorePerCorrect;
+        }if(answerFive===answers[4]){
+      score += scorePerCorrect;
+        }
+      }
       $("input[type=radio][name=questionOneoption]:checked").prop("checked", false);
       $("input[type=radio][name=questionTwooption]:checked").prop("checked", false);
       $("input[type=radio][name=questionThreeoption]:checked").prop("checked", false);
@@ -35,6 +37,7 @@ $("document").ready(function(){
       $("input[type=radio][name=questionFiveoption]:checked").prop("checked", false);
       $("#questionsIncomplete").text("");
       $("#result").text(percentage(score));
+
        event.preventDefault();
      });
   });
