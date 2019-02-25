407d41c60ebd47b422d2150bc246392b7d89ae8e
prudence naliaka <prudencenaliaka@gmail.com>
2019-02-25 08:34:06 +0300

    made changes in my js/scripts.js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,21 @@
 var answers=["a library of js","append","Document Object Model","front-end logic","slideUp,fadeIn,delay"];
-var scorePerCorrect=25;
-function percentage(){
-return "yourscore is" + parseInt(100) + "%";
-}
+var scorePerCorrect=20;
 
-$("document").ready(function(){
+$(document).ready(function(){
   $("#questions").submit(function(event){
-    $("#result").text("");
+
     var score=0;
-    var answerOne=($("input[type=radio][name=questionOneanswer]:checked").val());
-    var answerTwo=($("input[type=radio][name=questionTwoanswer]:checked").val());
-    var answerThree=($("input[type=radio][name=questionThreeanswer]:checked").val());
-    var answerFour=($("input[type=radio][name=questionFouranswer]:checked").val());
-    var answerFive=($("input[type=radio][name=questionFiveanswer]:checked").val());
+    var answerOne=($("input:radio[name=questionOneanswer]:checked").val());
+    var answerTwo=($("input:radio[name=questionTwoanswer]:checked").val());
+    var answerThree=($("input:radio[name=questionThreeanswer]:checked").val());
+    var answerFour=($("input:radio[name=questionFouranswer]:checked").val());
+    var answerFive=($("input:radio[name=questionFiveanswer]:checked").val());
 
     if(answerOne===undefined || answerTwo===undefined || answerThree===undefined || answerFour===undefined || answerFive===undefined){
     $("#questionsIncomplete").text("work out all questions first");
-    $("#questionsComplete").slideUp();
+    $("#questionsIncomplete").slideUp(2000);
+
+
       }
     else{
        if(answerOne===answers[0]){
@@ -29,10 +28,16 @@ $("document").ready(function(){
     score += scorePerCorrect;}
        if(answerFive===answers[4]){
     score += scorePerCorrect;}
+
     $("#questionsIncomplete").text("");
     $("#result").text(percentage(score));
+
     }
+    function percentage(score){
+    return "yourscore is " + (score) + "%";
+    }
+
        event.preventDefault();
-      
+
      });
   });
